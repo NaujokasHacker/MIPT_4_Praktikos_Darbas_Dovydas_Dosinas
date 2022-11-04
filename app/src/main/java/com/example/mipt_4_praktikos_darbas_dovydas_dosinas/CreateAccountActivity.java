9673a447089dcece5f2ed092ee889f16ef7676fe
Dovydas Dosinas <dovydas.dosinas@knf.stud.vu.lt>
2022-11-04 18:29:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mipt_4_praktikos_darbas_dovydas_dosinas/CreateAccountActivity.java
@@ -0,0 +1,30 @@
+package com.example.mipt_4_praktikos_darbas_dovydas_dosinas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText, passwordEditText, confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+
+
+
+    }
+}
